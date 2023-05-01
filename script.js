d5e78c4fc1b9cd4f0390acf9220651fa25831898
Eric Joshua Stangeland <StangelandEricJoshua@gmail.com>
2023-05-01 10:32:13 +0200

    minor changes

--- script.js
+++ script.js
@@ -141,9 +141,16 @@ function gameOverfunc() {
   document.removeEventListener("keyup", keyupHandler);
 }
 function shoot() {
-    /* to create a shoot function, we need to a new css element to be generated with id "bullet" that 
-    would have some movement speed going in the direction "forward" of the "player" id element
-     */
+  /* to create a shoot function, we need to a new css element to be generated with id "bullet" that 
+  would have some movement speed going in the direction "forward" of the "player" id element
+   */
+  //the code below occurs whenever the player presses "w" keyup
+  document.addEventListener("keyup", function(event) {
+    if (event.key === "w") {
+      bullet();
+    }
+  });
+  function bullet(){
     const bullet = document.createElement("img");
     bullet.setAttribute("id", "bullet");
     bullet.setAttribute("src", "assets/bullet.png");
@@ -152,5 +159,6 @@ function shoot() {
     bullet.setAttribute("x", player.getAttribute("x"));
     bullet.setAttribute("y", player.getAttribute("y"));
     body.appendChild(bullet);
+  }
 }
 
